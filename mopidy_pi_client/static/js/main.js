ab0133054f21f43ee304aeb42f0d76a99125c6dc
Stephen Kirkby <mail@stephenkirkby.co.uk>
2016-10-11 19:03:13 +0100

    Most working so far

--- mopidy_pi_client/static/js/main.js
+++ mopidy_pi_client/static/js/main.js
@@ -1,13 +1,38 @@
-var showAlbumInfo = function(album, tracks) {
-    if (tracks) {
-        for (var i = 0; i < tracks.length; i++) {
-            $("<a/>")
-                .text("Track no: " + tracks[i].track_no + ", track: " + tracks[i].name)
-                .appendTo($("#track-list"));
-        }
+/*
+ * Controller related methods to show screens.
+ */
 
-        // Set the album info.
-        $("#album-info .album-name").text(album.name);
+var showScreen = function(screenName, params) {
+    if (screenName == 'album-list') {
+        prettyLog("About to send the user to the album list screen");
+        getAlbumTrackData(function(albumTrackData) {
+            // Parse the album list from the data and finally render the album list.
+            renderAlbumList(parseAlbumData(albumTrackData));
+        });
+    } else if (screenName == "album-info") {
+        prettyLog("About to send the user to the album info screen, with URI", params.albumUri);
+        getAlbumTrackData(function(albumTrackData) {
+            renderAlbumInfo(albumTrackData[params.albumUri]);
+        });
+    }
+}
+
+/*
+ * Render methods, to render HTML from data.
+ */
+
+var renderAlbumInfo = function(tracks) {
+    prettyLog("About to render the following tracks", tracks);
+    if (tracks) {
+        $.each(tracks, function(index, track) {
+            $("<div/>")
+                .text("Track no: " + track.track_no + ", track: " + track.name)
+                .appendTo($('#album-info .track-list'));
+
+            // Set the album info.
+            $("#album-info .album-name").text(track.album.name);
+        });
+
 
         // Show our album-info screen, hide other screens.
         $(".screen").hide();
@@ -15,14 +40,17 @@ var showAlbumInfo = function(album, tracks) {
     }
 }
 
-var showAlbums = function(albums) {
-    if (albums) {
-        for (var i = 0; i < albums.length; i++) {
-            $("<a/>")
-                .text("Album: " + albums[i].name + ", artist: " + albums[i].artists[0])
-                .click(showScreen("album-info", { "album": albums[i] } ))
+var renderAlbumList = function(albums) {
+    prettyLog("About to render the following albums", albums);
+    if (albums != null) {
+        $.each(albums, function(index, album) {
+            $("<div/>")
+                .text("Album: " + album.name)
+                .click(function() {
+                    showScreen("album-info", {albumUri: album.uri } );
+                })
                 .appendTo($("#album-list"));
-        }
+        });
 
         // Show our album list screen, hide other screens.
         $(".screen").hide();
@@ -30,36 +58,68 @@ var showAlbums = function(albums) {
     }
 }
 
-var populateAlbumData = function(albumsRefs) {
-    albumData = [];
+/*
+ * Domain based data manipulation methods.
+ */
+
+var getAlbumTrackData = function(callback) {
+    // If we need to, first fetch the album track data.
+    if (typeof albumTrackData === 'undefined') {
+        prettyLog("Fetching album track data");
+        albumTrackData = [];
+
+        // The API provides no way of getting all album info in one go :(
+        mopidy.library.browse("local:directory?type=album")
+            .then(lookupAlbumUris)
+            .then(function(data) {
+                albumTrackData = data;
+                return albumTrackData;
+            })
+            .done(callback);
+    } else {
+        // Parse the album list from the data and finally render the album list.
+        prettyLog("Already got data");
+        callback(albumTrackData);
+    }
+}
+
+var lookupAlbumUris = function(albumRefs) {
+    var albumRefUris = [];
     if (albumRefs) {
-        // Loop through the album refs, getting data on each.
+        // Loop through the album refs, storing the uri for each.
         $.each(albumRefs, function(index, albumRef) {
-            // Request info on this album. Unfortunately the API only gives us access to album info through tracks :(
-            mopidy.library.lookup(null, albumRef.uri).then(function(tracksObj) {
-                var tracks = tracksObj[albumRef.uri];
-                if (tracks.length > 0) {
-                    console.log(tracks[0]);
-                }
-            });
+            albumRefUris.push(albumRef.uri);
         });
     }
+
+    // Request info on all the albums. Unfortunately the API only gives us access to album info through tracks :(
+    return mopidy.library.lookup(null, albumRefUris);
 }
 
-var showScreen = function showScreen(screenName, params) {
-    if (screenName == "album-list") {
-        // Fetch all the album data if we don't have it - the API provides no way of getting all album info in one go :(
-        if (albumData.length == 0) {
-            mopidy.library.browse("local:directory?type=album").done(populateAlbumData);
-        }
+var parseAlbumData = function(albumTrackData) {
+    var albumData = {};
+    if (albumTrackData) {
+        $.each(albumTrackData, function(index, trackObjs) {
+            if (trackObjs.length > 0) {
+                var album = trackObjs[0].album;
+                albumData[album.name] = album;
+            }
+        });
+    }
+    return albumData;
+}
 
-    } else if (screenName == "album-info") {
-        // Get the track list and show it as part of the album info.
-        mopidy.library.lookup("None", [ params.album.uri ]).done(showAlbumInfo(params.album));
+var prettyLog = function(message, obj) {
+    if (typeof obj !== 'undefined') {
+        console.log("pi-client: " + message + " " + JSON.stringify(obj, null, 2));
+    } else {
+        console.log("pi-client: " + message);
     }
 }
 
-// JQuery document ready.
+/*
+ * JQuery document ready, where things start.
+ */
 $(function() {
     mopidy = new Mopidy();
 
@@ -72,10 +132,7 @@ $(function() {
         $("#offline").hide();
         $("#online").show();
 
-        // Initialise our album data with no albums.
-        albumData = [];
-
-        // Initially show the album list screen.
-        showScreen("album-list", {});
+        // Show the album list as the default screen.
+        showScreen("album-list");
     });
 })
